2ec797cca44531f91eb934311234cf22dec80e6f
JanineZielman <44554453+JanineZielman@users.noreply.github.com>
2022-10-04 18:24:11 +0200

    mobile adjustments

--- slices/Quote/index.js
+++ slices/Quote/index.js
@@ -6,11 +6,7 @@ const Quote = ({ slice }) => {
     <section className="quote">
       <div className="container">
         {prismicH.isFilled.richText(slice.primary.quote) && (
-            <>
-              <span>&ldquo;</span>
-              <PrismicText field={slice.primary.quote} />
-              <span>&rdquo;</span>
-            </>
+          <PrismicText field={slice.primary.quote} />
         )}
       </div>
     </section>
