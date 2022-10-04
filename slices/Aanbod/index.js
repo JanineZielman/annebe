61cd5e164a0d0246785402bf39788c533f61250c
JanineZielman <44554453+JanineZielman@users.noreply.github.com>
2022-10-04 18:18:45 +0200

    homepage

--- slices/Aanbod/index.js
+++ slices/Aanbod/index.js
@@ -1,31 +1,33 @@
 import React from 'react'
-import { PrismicRichText } from '@prismicio/react'
+import { PrismicLink, PrismicRichText } from '@prismicio/react'
 
-const Aanbod = ({ slice }) => (
-  <section>
-    <span className="title">
-      {
-        slice.primary.title ?
-        <PrismicRichText field={slice.primary.title}/>
-        : <h2>Template slice, update me!</h2>
-      }
-    </span>
-    {
-      slice.primary.description ?
-      <PrismicRichText field={slice.primary.description}/>
-      : <p>start by editing this slice from inside Slice Machine!</p>
-    }
-    <style jsx>{`
-        section {
-          max-width: 600px;
-          margin: 4em auto;
-          text-align: center;
-        }
-        .title {
-          color: #8592e0;
-        }
-    `}</style>
-  </section>
-)
+const Aanbod = ({ slice }) => {
+  return(
+    <section className='aanbod'>
+      <div className='container'>
+        <h1>{slice.primary.titel}</h1>
+        <div className='flex'>
+          {slice.items?.map((item, i) => {
+            return(
+              <div className='item' key={`item-${i}`}>
+                <img src={item.afbeelding?.url}/>
+                <h2>{item.titel}</h2>
+                <span>{item.onder_titel}</span>
+                 {item.link &&
+                    <div className="read-more">
+                      <PrismicLink field={item.link}>
+                        <div className='bg'></div>
+                        {item.link_label}
+                      </PrismicLink>
+                    </div>
+                  }
+              </div>
+            )
+          })}
+        </div>
+      </div>
+    </section>
+  )
+}
 
 export default Aanbod
